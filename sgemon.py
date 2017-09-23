18925d0b35a67b70050d4896e254304744ab90da
Phil Corrick <philip.corrick@gmail.com>
2017-09-23 20:56:33 +0800

    Updated

--- sgemon.py
+++ sgemon.py
@@ -3,23 +3,22 @@ import datetime
 import urllib2
 import urllib
 import json
-from StringIO import StringIO
+from StringIO import StringIO    
 import sys
 import httplib
 import math
 from influxdb import InfluxDBClient
 
-
-client = InfluxDBClient('IP ADDRESS',8086,'LOGIN','PASSWORD','DB')
+client = InfluxDBClient('10.10.10.50',8086,'root','root','solar')
 
 interval = 300
 
 # Sungrow IDs
 # Fiddle with the sungrow solarinfo web interface till you get them
-sgUnitId = "xxxx"
-sgDeviceId = "xxxxxxxxx"
-sgUserId = "xxxx"
-sgPlantId = "xxxx"
+sgUnitId = "8603"
+sgDeviceId = "974894282"
+sgUserId = "7544"
+sgPlantId = "7811"
 sgDelay = 300 # delay between calls to Sungrow website
 
 # Sungrow Monitor Codes
@@ -39,87 +38,88 @@ sgDelay = 300 # delay between calls to Sungrow website
 # 128 AC frequency
 
 def round_minutes(dt, direction, resolution):
-    new_minute = (dt.minute // resolution + (1 if direction == 'up' else 0)) * resolution
-    newDatetime = dt + datetime.timedelta(minutes=new_minute - dt.minute)
-    return newDatetime
+	new_minute = (dt.minute // resolution + (1 if direction == 'up' else 0)) * resolution
+	newDatetime = dt + datetime.timedelta(minutes=new_minute - dt.minute)
+	return newDatetime
 
-#Test round_minutes
 #newDatetime = round_minutes(datetime.datetime.now(), "down", 15)
 #print "Hour:Minutes = "+str(newDatetime.hour)+":"+str(newDatetime.minute)
 
 def getCurrentPower():
-    url = 'http://www.solarinfobank.com/aapp/UnitDevices?uid='+sgUnitId+'&lan=en-us'
-    response = urllib2.urlopen(url)
-    urldata = response.read()
-    dataset  = json.loads(urldata)
+	url = 'http://www.solarinfobank.com/aapp/UnitDevices?uid='+sgUnitId+'&lan=en-us'
+	response = urllib2.urlopen(url)
+	urldata = response.read()
+	dataset  = json.loads(urldata)
 
-    s = dataset['units'][0]['devices'][0]['displayField']
+	s = dataset['units'][0]['devices'][0]['displayField']
 
-    power = int(s.split(':')[1].strip()[:-1])
-    return power
+	power = int(s.split(':')[1].strip()[:-1])
+	return power
 
 def getInverterTemps():
-    start = time.strftime("%Y%m%d") + "00"
-    end = time.strftime("%Y%m%d") + "23"
-    url = 'http://www.solarinfobank.com//aapp/MonitorDayChart?dId='+sgDeviceId+'&startYYYYMMDDHH='+start+'&endYYYYMMDDHH='+end+'&chartType=line&monitorCode=106&lan=en-us'
-    response = urllib2.urlopen(url)
-    urldata = response.read()
-    dataset  = json.loads(urldata)
+	start = time.strftime("%Y%m%d") + "00"
+	end = time.strftime("%Y%m%d") + "23"
+	url = 'http://www.solarinfobank.com//aapp/MonitorDayChart?dId='+sgDeviceId+'&startYYYYMMDDHH='+start+'&endYYYYMMDDHH='+end+'&chartType=line&monitorCode=106&lan=en-us'
+	response = urllib2.urlopen(url)
+	urldata = response.read()
+	dataset  = json.loads(urldata)
 
-    if(dataset['isHasData']):
-        return dataset
-    else:
-        return False
+	if(dataset['isHasData']):
+		return dataset
+	else:
+		return False
 
 def getGridVoltage():
-    start = time.strftime("%Y%m%d") + "00"
-    end = time.strftime("%Y%m%d") + "23"
-    url = 'http://www.solarinfobank.com//aapp/MonitorDayChart?dId='+sgDeviceId+'&startYYYYMMDDHH='+start+'&endYYYYMMDDHH='+end+'&chartType=line&monitorCode=116&lan=en-us'
-    response = urllib2.urlopen(url)
-    urldata = response.read()
-    dataset  = json.loads(urldata)
+	start = time.strftime("%Y%m%d") + "00"
+	end = time.strftime("%Y%m%d") + "23"
+	url = 'http://www.solarinfobank.com//aapp/MonitorDayChart?dId='+sgDeviceId+'&startYYYYMMDDHH='+start+'&endYYYYMMDDHH='+end+'&chartType=line&monitorCode=116&lan=en-us'
+	response = urllib2.urlopen(url)
+	urldata = response.read()
+	dataset  = json.loads(urldata)
 
-    if(dataset['isHasData']):
-        return dataset
-    else:
-        return False
+	if(dataset['isHasData']):
+		return dataset
+	else:
+		return False
 
 def getData(dataset):
-    roundedDatetime = round_minutes(datetime.datetime.now(), "down", 15)
-    t = str(roundedDatetime.hour).zfill(2)+":"+str(roundedDatetime.minute).zfill(2)
-    print t
+	roundedDatetime = round_minutes(datetime.datetime.now(), "down", 15)
+	t = str(roundedDatetime.hour).zfill(2)+":"+str(roundedDatetime.minute).zfill(2)
+	print t
 
-    categories, data = dataset['categories'], dataset['series'][0]['data']
-    index = categories.index(t)
-    datapoint = data[index - 1]
-    return datapoint
-        
-while True:
-    try:
-        power = getCurrentPower()
-        #print "Current Power: "+str(power)+" W"
+	categories, data = dataset['categories'], dataset['series'][0]['data']
+	index = categories.index(t)
+	datapoint = data[index - 1]
+	return datapoint
 
-        #temp = getData(getInverterTemps())
+try:
+	power = getCurrentPower()
+	#print "Current Power: "+str(power)+" W"
+	#temp = getData(getInverterTemps())
 
-        gridVoltage = getData(getGridVoltage())
-        #print gridVoltage
+	gridVoltage = getData(getGridVoltage())
+	#print gridVoltage
 
-        json_body = [
-            {
-                "measurement": "solar",
-                "tags": {
-                    "location": "rooftop solar"
-                },
-                "fields": {
-                    "power": power,
-                    "grid voltage": gridVoltage
-                }
-            }
-        ]
-        client.write_points(json_body)
-        time.sleep(interval)
-    except StandardError as e:
-        print "something went bad..."
-        print e.args
-        time.sleep(300)
+	json_body = [
+		{
+			"measurement": "solar",
+			"tags": {
+				"location": "rooftop solar"
+			},
+			"fields": {
+				"power": power,
+				"grid voltage": gridVoltage
+			}
+		}
+	]
+	client.write_points(json_body)
+
+except urllib2.URLError as e:
+	print "One of the web servers is dead, sleeping for a bit"
+	print e.args
+
+except StandardError as e:
+	print "something went bad... lets take a nap"
+	print e.args
+
 
